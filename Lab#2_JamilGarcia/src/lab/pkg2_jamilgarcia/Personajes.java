5a6f92aa5bfab532b8b06e247a6c2d2ea4ac62ff
JamilGarcia <you@example.com>
2020-01-31 14:58:08 -0600

    Se creo el constructor y los Getters & Setters

--- Lab#2_JamilGarcia/src/lab/pkg2_jamilgarcia/Personajes.java
+++ Lab#2_JamilGarcia/src/lab/pkg2_jamilgarcia/Personajes.java
@@ -3,5 +3,138 @@ package lab.pkg2_jamilgarcia;
 
 public class Personajes {
     
+    private String tipo;
+    private String nombre;
+    private String raza;
+    private double estatura;
+    private double peso;
+    private int años;
+    private String descripcion;
+    private String nacion;
+    private int HP;
+    private int CS;
+    private int AC;
+    private int DG;
+    
+    public Personajes(){
+        
+    }
+
+    public Personajes(String tipo, String nombre, String raza, double estatura, double peso, int años, String descripcion, String nacion, int HP, int CS, int AC, int DG) {
+        this.tipo = tipo;
+        this.nombre = nombre;
+        this.raza = raza;
+        this.estatura = estatura;
+        this.peso = peso;
+        this.años = años;
+        this.descripcion = descripcion;
+        this.nacion = nacion;
+        this.HP = HP;
+        this.CS = CS;
+        this.AC = AC;
+        this.DG = DG;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public double getEstatura() {
+        return estatura;
+    }
+
+    public void setEstatura(double estatura) {
+        this.estatura = estatura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public int getAños() {
+        return años;
+    }
+
+    public void setAños(int años) {
+        this.años = años;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getNacion() {
+        return nacion;
+    }
+
+    public void setNacion(String nacion) {
+        this.nacion = nacion;
+    }
+
+    public int getHP() {
+        return HP;
+    }
+
+    public void setHP(int HP) {
+        this.HP = HP;
+    }
+
+    public int getCS() {
+        return CS;
+    }
+
+    public void setCS(int CS) {
+        this.CS = CS;
+    }
+
+    public int getAC() {
+        return AC;
+    }
+
+    public void setAC(int AC) {
+        this.AC = AC;
+    }
+
+    public int getDG() {
+        return DG;
+    }
+
+    public void setDG(int DG) {
+        this.DG = DG;
+    }
+
+    @Override
+    public String toString() {
+        return "Personajes{" + "tipo=" + tipo + ", nombre=" + nombre + ", raza=" + raza + ", estatura=" + estatura + ", peso=" + peso + ", a\u00f1os=" + años + ", descripcion=" + descripcion + ", nacion=" + nacion + ", HP=" + HP + ", CS=" + CS + ", AC=" + AC + ", DG=" + DG + '}';
+    }
+    
     
 }
