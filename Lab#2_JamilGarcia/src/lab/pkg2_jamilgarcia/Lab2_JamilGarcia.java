5d9cb3054e4eb92d8fdc6aa31124cfa78b7ceda6
JamilGarcia <you@example.com>
2020-01-31 15:01:56 -0600

    Se crearon los personajes predefinidos

--- Lab#2_JamilGarcia/src/lab/pkg2_jamilgarcia/Lab2_JamilGarcia.java
+++ Lab#2_JamilGarcia/src/lab/pkg2_jamilgarcia/Lab2_JamilGarcia.java
@@ -14,6 +14,61 @@ public class Lab2_JamilGarcia {
         ArrayList per = new ArrayList();
         //Generacion de los personajes predefinidos
         
+        //Personaje 1
+        per.add(new Personajes());
+        ((Personajes) per.get(0)).setTipo("Clerigo");
+        ((Personajes) per.get(0)).setNombre("Clerigo Maestro");
+        ((Personajes) per.get(0)).setRaza("Humano");
+        ((Personajes) per.get(0)).setEstatura(1.80);
+        ((Personajes) per.get(0)).setPeso(140.56);
+        ((Personajes) per.get(0)).setAños(34);
+        ((Personajes) per.get(0)).setDescripcion("Tal como su nombre lo dice, es un Clerigo Maestro, yo que tu no lo subestimaria, no sabemos si tiene trucos bajo la manga");
+        ((Personajes) per.get(0)).setNacion("Zebes");
+        ((Personajes) per.get(0)).setHP(50);
+        ((Personajes) per.get(0)).setCS(97);
+        ((Personajes) per.get(0)).setAC(40);
+        
+        //Personaje 2
+        per.add(new Personajes());
+        ((Personajes) per.get(1)).setTipo("Barbaro");
+        ((Personajes) per.get(1)).setNombre("Barabro Enrrabiado");
+        ((Personajes) per.get(1)).setRaza("Enano");
+        ((Personajes) per.get(1)).setEstatura(1.30);
+        ((Personajes) per.get(1)).setPeso(80.96);
+        ((Personajes) per.get(1)).setAños(27);
+        ((Personajes) per.get(1)).setDescripcion("Será un enano pero por su altura, juventud, peso y rabiadez su agilidad es muy buena, ten cuidado!!!");
+        ((Personajes) per.get(1)).setNacion("Norfair");
+        ((Personajes) per.get(1)).setHP(50);
+        ((Personajes) per.get(1)).setCS(93);
+        ((Personajes) per.get(1)).setAC(65);
+        
+        //Personaje 3
+        per.add(new Personajes());
+        ((Personajes) per.get(2)).setTipo("Mago");
+        ((Personajes) per.get(2)).setNombre("MessiGishian");
+        ((Personajes) per.get(2)).setRaza("Mediano");
+        ((Personajes) per.get(2)).setEstatura(1.73);
+        ((Personajes) per.get(2)).setPeso(134.90);
+        ((Personajes) per.get(2)).setAños(32);
+        ((Personajes) per.get(2)).setDescripcion("Si es un mago, si es uno de los mejores del mundo, ten cuidado su habilidad de sus pies no es algo que cualquier defensa pueda controlar");
+        ((Personajes) per.get(2)).setNacion("Crateria");
+        ((Personajes) per.get(2)).setHP(56);
+        ((Personajes) per.get(2)).setCS(101);
+        ((Personajes) per.get(2)).setAC(20);
+        
+        //Personaje 4
+        per.add(new Personajes());
+        ((Personajes) per.get(3)).setTipo("Picaro");
+        ((Personajes) per.get(3)).setNombre("Él...");
+        ((Personajes) per.get(3)).setRaza("Elfo");
+        ((Personajes) per.get(3)).setEstatura(2.30);
+        ((Personajes) per.get(3)).setPeso(200.99);
+        ((Personajes) per.get(3)).setAños(68);
+        ((Personajes) per.get(3)).setDescripcion("Él simplemente es Él, no necesita Presentacion, solo teme");
+        ((Personajes) per.get(3)).setNacion("Brinstar");
+        ((Personajes) per.get(3)).setHP(70);
+        ((Personajes) per.get(3)).setCS(80);
+        ((Personajes) per.get(3)).setAC(50);
         
         char resp = 's';
         while (resp == 's'){
